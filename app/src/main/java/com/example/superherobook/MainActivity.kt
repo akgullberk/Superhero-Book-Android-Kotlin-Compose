3beeb7ee8378017e636b8249d3784b0085f1b2ef
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-26 15:38:34 +0300

    Detail screen

--- app/src/main/java/com/example/superherobook/MainActivity.kt
+++ app/src/main/java/com/example/superherobook/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,15 +12,20 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superherobook.ui.theme.SuperHeroBookTheme
@@ -45,7 +51,7 @@ class MainActivity : ComponentActivity() {
         val superman = Superhero("Superman","DC",R.drawable.superman)
         val batman = Superhero("Batman","DC",R.drawable.batman)
         val aquaman = Superhero("Aquaman","DC",R.drawable.aquaman)
-        val ironman = Superhero("Ironman","Marvel",R.drawable.ironman)
+        val ironman = Superhero("Ironman","Marvel", R.drawable.ironman)
         val deadpool = Superhero("Deadpool","Marvel",R.drawable.deadpool)
 
         superheroList.add(superman)
@@ -57,14 +63,36 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-
-
-
-@Preview(showBackground = true)
 @Composable
-fun SuperheroPreview() {
-    SuperHeroBookTheme {
+fun DetailScreen(superhero : Superhero){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = MaterialTheme.colorScheme.primaryContainer),
+        contentAlignment = Alignment.Center
+    ){
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = superhero.name,
+                    style = MaterialTheme.typography.displayLarge,
+                modifier = Modifier.padding(2.dp),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+                )
 
+            Image(bitmap = ImageBitmap.imageResource(id = superhero.image),
+                contentDescription = superhero.name,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(300.dp,250.dp)
+
+                )
+
+            Text(text = superhero.universe,
+                style = MaterialTheme.typography.displaySmall,
+                modifier = Modifier.padding(2.dp),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
+
